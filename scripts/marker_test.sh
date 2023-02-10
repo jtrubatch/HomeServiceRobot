532e16a6dbe24e82c2ad5e1ea06c46996bfb5b47
jtrubatch <105450295+jtrubatch@users.noreply.github.com>
2023-02-10 08:32:14 -0800

    updated to include the running of the mapserver node

--- scripts/marker_test.sh
+++ scripts/marker_test.sh
@@ -2,6 +2,8 @@
 
 xterm -e " roslaunch my_robot world.launch" &  # Contains rviz launch and config
 sleep 6
+xterm -e " roslaunch my_robot amcl.launch" &
+sleep 6
 xterm -e " rosrun add_markers basic_shapes" 
 
 
